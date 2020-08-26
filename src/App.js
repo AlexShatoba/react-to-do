2a7989f1b5b66d5caeca392978268574b6d61c29
AlexShatoba <alexander.shatoba@saritasa.com>
2020-08-26 18:51:03 +0700

    New app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListElement from './components/list-element/list-element'
+
+class App extends Component {
+  render() {
+    return (
+      <ListElement />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/list-element/list-element.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const ListElement = () => {
+  return (
+    <h1>Hola</h1>
+  )
+}
+
+export default ListElement;
